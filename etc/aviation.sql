f0c8ae1d31b800f8cb23a9f17d6fe11bfd289d3d
Dionysis Zindros <dionyziz@gmail.com>
2012-01-16 10:40:55 +0000

    MySQL schema

---
+++ etc/aviation.sql
@@ -0,0 +1,51 @@
+CREATE TABLE checks (
+    chkid INT NOT NULL REFERENCES checktypes(chkid),
+    pid INT NOT NULL REFERENCES planes(pid),
+    umn INT NOT NULL REFERENCES techs(umn),
+    created DATETIME,
+    duration INT NOT NULL,
+    score INT NOT NULL,
+    PRIMARY KEY (chkid,pid,umn),
+    CHECK score > (SELECT maxscore FROM checktypes t WHERE chkid = t.chkid)
+);
+CREATE TABLE checktypes (
+    chkid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name TEXT,
+    maxscore INT NOT NULL
+);
+CREATE TABLE employees (
+    umn INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ssn INT UNIQUE,
+    name TEXT NOT NULL,
+    phone INT,
+    addr TEXT,
+    salary INT
+);
+CREATE TABLE planes (
+    pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tid INT REFERENCES types(tid)
+);
+CREATE TABLE regulators (
+    umn INT NOT NULL PRIMARY KEY REFERENCES employees(umn),
+    checked DATETIME
+);
+CREATE TABLE specializations (
+    umn INT NOT NULL REFERENCES techs(umn),
+    tid INT NOT NULL REFERENCES types(tid),
+    PRIMARY KEY (umn, tid)
+);
+CREATE TABLE techs (
+    umn INT NOT NULL PRIMARY KEY REFERENCES employees(umn)
+);
+CREATE TABLE types (
+    tid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    capacity INT,
+    weight INT
+);
+CREATE TABLE users (
+    uid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(32),
+    password CHAR(64),
+    passwordsalt CHAR(8),
+    email VARCHAR(64)
+);
