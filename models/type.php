10f050ee6bb9d082158bc5f1c2c9214eb471ec02
Gregory Liras <gregliras@gmail.com>
2012-01-17 19:26:41 +0200

    cleaner indexing, no slicing

--- models/type.php
+++ models/type.php
@@ -19,7 +19,7 @@
         );
         $rows = array();
         while ( $row = mysql_fetch_array( $res ) ) {
-            $rows[$row[0]] = array_slice($row,1);
+            $rows[ $row[ ' tid' ] ] = $row;
         }
         return $rows;
     }
