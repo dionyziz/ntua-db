10098fad14c69301568fafb3320c70b5a80128f4
indorilftw <indoril@europa.kamibu.com>
2012-01-17 17:44:22 +0000

    Try/Catch complete, employees ready

--- models/employee.php
+++ models/employee.php
@@ -1,17 +1,24 @@
 <?php
-    function employeeCreate( $ssn, $name, $phone, $addr, $salary ) {
-        db(
-            "INSERT INTO
-                employees
-            SET
-				ssn = :ssn,
-                name = :name,
-                phone = :phone,
-                addr = :addr,
-				salary = :salary",
-            compact( 'ssn', 'name', 'phone', 'addr', 'salary' )
-        );
+    function employeeCreate( $ssn, $name, $phone, $addr, $salary, $errors ) {
+                try{
+                    db(
+                        "INSERT INTO
+                            employees
+                        SET
+                            ssn = :ssn,
+                            name = :name,
+                            phone = :phone,
+                            addr = :addr,
+                            salary = :salary",
+                            compact( 'ssn', 'name', 'phone', 'addr', 'salary' )
+                    );
+                }
+                catch ( DBException $e ) {
+                    $errors[] = 'duplicatessn';
+                    Redirect( 'employee/create?errors=' . implode( ',', $errors ) . '&name=' . $name . '&phone=' . $phone . '&addr=' . $addr . '&salary=' . $salary );
+                }
     }
+
     function employeeListing() {
         $res = db(
             "SELECT
