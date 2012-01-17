351a3e2ee94fd56ceaaaf1fa5d3bcf4d18ae0ca6
Gregory Liras <gregliras@gmail.com>
2012-01-17 16:44:34 +0200

    added plane

---
+++ views/plane/create.php
@@ -0,0 +1,29 @@
+Πληκτρολογήστε τις πληροφορίες του νέου αεροσκάφους:
+<form action='plane/create' method='post'>
+    <div>
+        <label>Κωδικός αεροσκάφους:</label> <input type='text' name='pid' value='<?php
+        echo htmlspecialchars( $pid );
+        ?>' <?php
+        if ( isset( $errors[ 'nopid' ] ) ) {
+            ?> class='error' <?php
+        }
+        ?> />
+    </div>
+    <div>
+    <label>Όνομα τύπου:</label> <select name='tid'
+        <?php
+        if ( isset( $errors[ 'notid' ] ) ) {
+            ?> class='error' <?php
+        }
+        ?> >
+        <?php
+        $types = typeListing();
+        foreach ( $types as $type) {
+            echo '<option value=' . $type[ 'tid' ] . '>' . $type[ 'name' ] .'</option>';
+        }
+
+        ?>
+        </select>
+    </div>
+    <input type='submit' value='Δημιουργία νέου αεροσκάφους' />
+</form>
