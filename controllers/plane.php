fc7224d00b64e04ae34498f655abc399249cd3c8
Gregory Liras <gregliras@gmail.com>
2012-01-19 12:20:28 +0200

    added check, updated plane

--- controllers/plane.php
+++ controllers/plane.php
@@ -1,27 +1,28 @@
 <?php
     class PlaneController extends Controller {
-        public function Listing() {
+        public static function Listing() {
             $planes = planeListing();
             $types = typeListing();
             view( 'plane/listing', array( 'planes' => $planes , 'types' => $types ) );
         }
-        public function createView( $errors, $pid, $tid ) {
-            $errors = array_flip( explode( ',', $errors ) );
-            $types = typeListing();
-            view( 'plane/create', compact( 'errors', 'pid', 'tid', 'types' ) );
+        public static function createView( $errors, $pid, $tid ) {
+            if ( !empty( $pid ) ) {
+                $plane = planeItem( $pid );
+                if ( $plane === false ) {
+                    throw new Exception( 'The plane you are trying to edit does not exist' );
+                }
+                $errors = array_flip( explode( ',', $errors ) );
+                $types = typeListing();
+                view( 'plane/create', compact( 'errors', 'pid', 'tid', 'types' ) );
+            }
         }
-        public function create( $pid, $tid ) {
-            $errors = array();
-            if ( empty( $pid ) ) {
-                $errors[] = 'nopid';
-            }
-            if ( empty( $pid ) ) {
-                $errors[] = 'notid';
-            }
+        public static function create( $pid, $tid ) {
+            $vars = compact( 'pid', 'tid' );
             if ( !empty( $errors ) ) {
-                Redirect( 'plane/create?errors=' . implode( ',', $errors ) . '&pid=' . $pid . '&tid=' . $tid );
+                Redirect( 'plane/create?errors=' . implode( ',', $errors ) . '&' . Controller::paramURL( $vars ) );
             }
             planeCreate( $pid, $tid );
+            Redirect( 'type/listing' );
         }
         public static function delete( $pid ) {
             $vars = compact( 'pid' );
