e8a2edd24c1738af5816cfa5da63e701b396da22
Dionysis Zindros <dionyziz@gmail.com>
2012-01-23 13:11:28 +0000

    migrating to using static classes for models

--- models/employee.php
+++ models/employee.php
@@ -1,90 +1,59 @@
 <?php
-    function employeeCreate( $umn, $ssn, $name, $phone, $addr, $salary ) {
-        try {
-            db(
-                "INSERT INTO
-                    employees
-                SET
-                    umn = :umn,
-                    ssn = :ssn,
-                    name = :name,
-                    phone = :phone,
-                    addr = :addr,
-                    salary = :salary",
+    class Employee {
+        public static create( $umn, $ssn, $name, $phone, $addr, $salary ) {
+            try {
+                return db_insert(
+                    'employees',
                     compact( 'umn', 'ssn', 'name', 'phone', 'addr', 'salary' )
-            );
+                );
+            }
+            catch ( DBException $e ) {
+                throw new Duplicate();
+            }
         }
-        catch ( DBException $e ) {
-            throw new Duplicate();
-        }
-        return mysql_insert_id();
-    }
-
-    function employeeListing( $occ ) {
-        $res = db(
-            "SELECT
-                e.*, r.checked
-            FROM
-                employees e
-            LEFT JOIN
-                techs t
-            ON
-                t.umn = e.umn
+        public static listing( $occ ) {
+            $res = db(
+                "SELECT
+                    e.*, r.checked
+                FROM
+                    employees e
                 LEFT JOIN
-                    regulators r
+                    techs t
                 ON
-                    r.umn = e.umn
-            WHERE
-                ( :occ = 'tech' AND t.umn IS NOT NULL )
-                OR ( :occ = 'regulator' AND r.umn IS NOT NULL )
-                OR ( :occ = '' )
-            ORDER BY umn ",
-            compact( 'occ' )
-        );
-        $rows = array();
-        while ( $row = mysql_fetch_array( $res ) ) {
-            $rows[ $row[ 'umn' ] ] = $row;
+                    t.umn = e.umn
+                    LEFT JOIN
+                        regulators r
+                    ON
+                        r.umn = e.umn
+                WHERE
+                    ( :occ = 'tech' AND t.umn IS NOT NULL )
+                    OR ( :occ = 'regulator' AND r.umn IS NOT NULL )
+                    OR ( :occ = '' )
+                ORDER BY umn ",
+                compact( 'occ' )
+            );
+            $rows = array();
+            while ( $row = mysql_fetch_array( $res ) ) {
+                $rows[ $row[ 'umn' ] ] = $row;
+            }
+            return $rows;
         }
-        return $rows;
-    }
-    function employeeDelete( $umn ) {
-        db(
-            "DELETE FROM
-                employees
-            WHERE
-                umn = :umn
-            LIMIT 1",
-            compact( 'umn' )
-        );
-    }
-    function employeeUpdate( $umn, $ssn, $name, $phone, $addr, $salary, $imageid ) {
-        db(
-            "UPDATE
-                employees
-            SET
-                ssn = :ssn,
-                name = :name,
-                phone = :phone,
-                addr = :addr,
-                salary = :salary,
-                imageid = :imageid
-            WHERE
-                umn = :umn
-            LIMIT 1",
-            compact( 'umn', 'ssn', 'name', 'phone', 'addr', 'salary', 'imageid' )
-        );
-    }
-    function employeeItem( $umn ) {
-        $res = db(
-            "SELECT
-                *
-            FROM
-                employees
-            WHERE
-                umn = :umn
-            LIMIT 1",
-            compact( 'umn' )
-        );
-        return mysql_fetch_array( $res );
+        public static delete( $umn ) {
+            db_delete( 'employees', compact( 'umn' ) );
+        }
+        public static update( $umn, $ssn = false, $name = false, $phone = false, $addr = false, $salary = false, $imageid = false ) {
+            $fields = compact( 'ssn', 'name', 'phone', 'addr', 'salary', 'imageid' );
+
+            foreach ( $fields as $field => $value ) {
+                if ( $value === false ) {
+                    unset( $fields[ $field ] );
+                }
+            }
+
+            db_update( 'employees', compact( 'umn' ), $fields );
+        }
+        public static item( $umn ) {
+            return db_select( 'employees', compact( 'umn' ) );
+        }
     }
 ?>
