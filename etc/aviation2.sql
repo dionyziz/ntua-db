902bd6756bae75b47d6782a9c1ab95b444c006e1
indorilftw <vgerak@gmail.com>
2012-03-14 03:09:45 +0200

    Added second schema with checks, started report

--- etc/aviation2.sql
+++ etc/aviation2.sql
@@ -1,66 +1,63 @@
 CREATE TABLE checks (
-    checkid INT AUTO_INCREMENT PRIMARY KEY,
-    checktypeid INT NOT NULL,
-    pid INT NOT NULL,
-    umn INT,
+    checkid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    checktypeid UNSIGNED INT NOT NULL,
+    pid UNSIGNED INT NOT NULL,
+    umn UNSIGNED INT,
     created DATETIME,
-    duration INT NOT NULL,
-    score INT NOT NULL,
+    duration UNSIGNED INT NOT NULL,
+    score UNSIGNED INT NOT NULL,
     FOREIGN KEY ( checktypeid ) REFERENCES checktypes( checktypeid ) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY ( pid ) REFERENCES planes( pid ) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY ( umn ) REFERENCES techs( umn ) ON DELETE SET NULL ON UPDATE CASCADE, -- would prefer ON DELETE SET NULL ON UPDATE CASCADE here
-    CONSTRAINT chk_checks CHECK (score > ( SELECT maxscore FROM checktypes t WHERE checktypeid = t.checktypeid ) AND duration > 0 AND checkid > 0 )
+    CONSTRAINT chk_checks CHECK (score > ( SELECT maxscore FROM checktypes t WHERE checktypeid = t.checktypeid ) AND duration > 0 )
 ) ENGINE=InnoDB;
 CREATE TABLE checktypes (
-    checktypeid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    checktypeid UNSIGNED INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name TEXT,
-    maxscore INT NOT NULL
-	CHECK ( checktypeid > 0 )
+    maxscore UNSIGNED INT NOT NULL
 ) ENGINE=InnoDB;
 CREATE TABLE employees (
-    umn INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    ssn INT UNIQUE,
+    umn UNSIGNED INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ssn UNSIGNED INT UNIQUE,
     name TEXT NOT NULL,
     imageid INT DEFAULT NULL,
     phone VARCHAR( 32 ),
     addr TEXT,
-    salary INT
+    salary UNSIGNED INT
     FOREIGN KEY ( imageid ) REFERENCES images( imageid ) ON DELETE SET NULL ON UPDATE CASCADE
-	CONSTRAINT chk_employees CHECK ( umn > 0 AND ssn > 0 AND salary >= 0 )
 ) ENGINE=InnoDB;
 CREATE TABLE planes (
-    pid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tid INT NOT NULL,
+    pid UNSIGNED INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tid UNSIGNED INT NOT NULL,
     FOREIGN KEY ( tid ) REFERENCES planetypes( tid ) ON DELETE CASCADE ON UPDATE CASCADE
-	CHECK ( pid > 0 )
 ) ENGINE=InnoDB;
 CREATE TABLE regulators (
-    umn INT NOT NULL PRIMARY KEY,
+    umn UNSIGNED INT NOT NULL PRIMARY KEY,
     checked DATE NOT NULL,
     FOREIGN KEY ( umn ) REFERENCES employees( umn ) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 CREATE TABLE specializations (
-    umn INT NOT NULL,
-    tid INT NOT NULL,
+    umn UNSIGNED INT NOT NULL,
+    tid UNSIGNED INT NOT NULL,
     PRIMARY KEY ( umn, tid ),
     FOREIGN KEY ( umn ) REFERENCES techs( umn ) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY ( tid ) REFERENCES planetypes( tid ) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 CREATE TABLE techs (
-    umn INT NOT NULL PRIMARY KEY,
+    umn UNSIGNED INT NOT NULL PRIMARY KEY,
     FOREIGN KEY ( umn ) REFERENCES employees( umn ) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 CREATE TABLE planetypes (
     tid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR( 32 ) NOT NULL,
-    capacity INT DEFAULT NULL,
-    weight INT DEFAULT NULL,
+    capacity UNSIGNED INT DEFAULT NULL,
+    weight UNSIGNED INT DEFAULT NULL,
     imageid INT DEFAULT NULL,
     FOREIGN KEY ( imageid ) REFERENCES images( imageid ) ON DELETE SET NULL ON UPDATE CASCADE
-	CONSTRAINT chk_planetypes CHECK ( tid > 0 AND capacity > 0 AND weight > 0 )
+	CONSTRAINT chk_planetypes CHECK ( capacity > 0 AND weight > 0 )
 ) ENGINE=InnoDB;
 CREATE TABLE users (
-    uid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uid UNSIGNED INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(32),
     password CHAR(64),
     passwordsalt CHAR(8),
@@ -68,7 +65,7 @@ CREATE TABLE users (
 ) ENGINE=InnoDB;
 CREATE TABLE images (
     imageid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    size INT DEFAULT NULL,
-    width INT DEFAULT NULL,
-    height INT DEFAULT NULL
+    size UNSIGNED INT DEFAULT NULL,
+    width UNSIGNED INT DEFAULT NULL,
+    height UNSIGNED INT DEFAULT NULL
 ) ENGINE=InnoDB;
