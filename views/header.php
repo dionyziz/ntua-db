f81b4b10a312d44648623366131189e619b44781
Gregory Liras <gregliras@gmail.com>
2012-03-16 09:54:45 +0000

    updated views/header, added statistics and home

--- views/header.php
+++ views/header.php
@@ -24,8 +24,10 @@
     <body>
         <h1><a href=''><img src='images/logo.gif' alt='Athens International Airport'></a></h1>
         <ul id='navigation'>
+            <li><a href='/'>Αρχική</a></li>
             <li><a href='plane/listing'>Αεροσκάφη</a></li>
             <li><a href='check/listing'>Έλεγχοι</a></li>
             <li><a href='employee/listing'>Εργαζόμενοι</a></li>
+            <li><a href='statistic/listing'>Προεπισκόπηση</a></li>
         </ul>
         <div id='universe'>
